7ef9af0856de8bf8403e3efcc342d071dbde9089
polachade nantaruan <96508766+Gripenyuki@users.noreply.github.com>
2023-10-26 22:57:53 +0700

    add bug swap player

---
+++ Assets/dev scene/Program/swapPlayer.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swapPlayer : MonoBehaviour
+{
+    public List<GameObject> playerPrefabs;
+    public string playerTag = "Player";
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            DestroyAndInstantiatePlayer(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            DestroyAndInstantiatePlayer(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            DestroyAndInstantiatePlayer(2);
+        }
+    }
+
+    private void DestroyAndInstantiatePlayer(int prefabIndex)
+    {
+        GameObject playerToDestroy = GameObject.FindGameObjectWithTag(playerTag);
+
+        if (playerToDestroy != null)
+        {
+            Vector3 spawnPosition = playerToDestroy.transform.position;
+
+            Destroy(playerToDestroy);
+
+            StartCoroutine(InstantiatePlayerWithDelay(prefabIndex, spawnPosition));
+        }
+    }
+
+    private IEnumerator InstantiatePlayerWithDelay(int prefabIndex, Vector3 spawnPosition)
+    {
+        yield return new WaitForSeconds(1);
+
+        if (prefabIndex >= 0 && prefabIndex < playerPrefabs.Count)
+        {
+            Instantiate(playerPrefabs[prefabIndex], spawnPosition, Quaternion.identity);
+        }
+    }
+}
