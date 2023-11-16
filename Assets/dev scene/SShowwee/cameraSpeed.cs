db8cf1565ef3cc69e135b7234de3d3caa16a803d
polachade nantaruan <96508766+Gripenyuki@users.noreply.github.com>
2023-11-16 20:56:51 +0700

    ฟฟฟฟ

---
+++ Assets/dev scene/SShowwee/cameraSpeed.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraSpeed : MonoBehaviour
+{
+    public static float updatecameraspeed;
+    void Update()
+    {
+        
+    }
+}
